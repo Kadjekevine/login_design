32cc4567a76fa0620d4b97fb6395b6da36352ae6
Nathan dev <=>
2023-04-15 02:33:12 +0100

    Add firebase auth

---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Je t'aime encore",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.green[700],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('Signed in as ' + user.email!),
+          MaterialButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            color: Colors.red[500],
+            child: Text(
+              'Log out',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
